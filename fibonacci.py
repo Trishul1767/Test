369b350d2f78c6d2778196efb60e93d3a6f450ab
Trishul1767 <trishul1767@gmail.com>
2026-04-09 21:01:52 +0530

    python basics

--- fibonacci.py
+++ fibonacci.py
@@ -4,5 +4,19 @@ n=int(input())
 a=0
 b=1
 for i in range(n):
-    print(a)
+    print(a)   
     a,b=b,a+b
+#fibonacci series using recursion
+def fibonacci(n):
+    if n<=0:
+        return []
+    elif n==1:
+        return [0]
+    elif n==2:
+        return [0,1]
+    else:
+        fib_series=fibonacci(n-1)
+        fib_series.append(fib_series[-1]+fib_series[-2])
+        return fib_series
+n=int(input())
+print(fibonacci(n))
