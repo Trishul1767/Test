b9229197d89b01a33f815694dfdebf2e23c94680
Trishul1767 <trishul1767@gmail.com>
2026-04-26 19:33:30 +0530

    Set built in funtions

---
+++ Set built in funtions.py
@@ -0,0 +1,19 @@
+# Set built in funtions
+S=set()
+S.add(1)
+S.add(2)
+print(S)
+#more built in functions
+print(len(S))
+print(1 in S)
+print(3 in S)
+# set all built in functions
+print(dir(S))
+# set operations
+A={1,2,3}
+B={3,4,5}
+print(A.union(B))
+print(A.intersection(B))
+print(A.difference(B))
+print(A.symmetric_difference(B))
+
