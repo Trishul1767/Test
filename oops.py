ca5b2493ec2fc2c06ec188a4c1379f9fb3c6f192
Trishul1767 <trishul1767@gmail.com>
2026-05-08 21:09:57 +0530

    Add awake state and wake/sleep methods to Dog class

--- oops.py
+++ oops.py
@@ -11,7 +11,20 @@ class Dog:
     def __init__(self,name,age):
         self.name=name
         self.age=age
+        self.awake:bool=False
     def bark(self):
         print(f'{self.name} says Woof!')
+    def wake_up(self):
+        if self.awake:
+            print("Dog is already awake.")
+        else:
+            self.awake=True
+            print("Dog is now awake.")
+    def sleep(self):
+        if self.awake:
+            self.awake=False
+            print("Dog is now asleep.")
+        else:
+            print("Dog is already asleep.")
 d1=Dog('Buddy',3)
 d1.bark()
