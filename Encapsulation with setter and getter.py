4e2e3bdd2f63f60191ab31b791aa93c7a9782864
Trishul1767 <trishul1767@gmail.com>
2026-05-25 18:15:49 +0530

    Enhance Employee class with setter methods example
    Added an example of encapsulation using only setter methods in the Employee class.

--- Encapsulation with setter and getter.py
+++ Encapsulation with setter and getter.py
@@ -22,3 +22,22 @@ emp.set_name("Jane Doe")
 emp.set_salary(60000)
 print(emp.get_name())  # Output: Jane Doe
 print(emp.get_salary())  # Output: 60000
+
+
+
+#Encapsulation with only setter method
+class Employee:
+    def __init__(self, name, salary):
+        self.__name = name
+        self.__salary = salary
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_salary(self, salary):
+        self.__salary = salary
+emp = Employee("John Doe", 50000)
+emp.set_name("Jane Doe")
+emp.set_salary(60000)
+print(emp._Employee__name)  # Output: Jane Doe
+print(emp._Employee__salary)  # Output: 60000
