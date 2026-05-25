2f277457f53863f07bcf8eb273e66b2d2ddd4aaf
Trishul1767 <trishul1767@gmail.com>
2026-05-25 18:16:15 +0530

    Add getter methods for Employee class

--- Encapsulation with setter and getter.py
+++ Encapsulation with setter and getter.py
@@ -41,3 +41,20 @@ emp.set_name("Jane Doe")
 emp.set_salary(60000)
 print(emp._Employee__name)  # Output: Jane Doe
 print(emp._Employee__salary)  # Output: 60000
+
+
+#Encapsulation with only getter method
+class Employee:
+    def __init__(self, name, salary):
+        self.__name = name
+        self.__salary = salary
+
+    def get_name(self):
+        return self.__name
+
+    def get_salary(self):
+        return self.__salary
+emp = Employee("John Doe", 50000)
+print(emp.get_name())  # Output: John Doe
+print(emp.get_salary())  # Output: 50000
+
