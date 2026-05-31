26d142ad838a8e590af8aee9bd2a0653fd702e61
Trishul1767 <trishul1767@gmail.com>
2026-05-31 09:03:42 +0530

    Add more vehicles to the list in files.py

--- files.py
+++ files.py
@@ -1,4 +1,4 @@
-A=['Virtus', 'Santa Cruze', 'Specialized', 'Trek', 'porsche 918 Spyder', 'AstonMartin Vantage', 'Ferrari Laferrari', 'McLaren P1', 'McLaren720s', 'Ducati Panigale V4']
+A=['Virtus', 'Santa Cruze', 'Specialized', 'Trek', 'Porsche 918 Spyder', 'AstonMartin Vantage', 'Ferrari Laferrari', 'McLaren P1', 'McLaren720s', 'Ducati Panigale V4','Fuji','S-Works','Giant']
 with open('My_Garage', 'w') as f:
     for i in A:
         f.write(f"I will own a {i} one day\n")
