9190d2c736ac5a1d650f9241ec514d2876767267
Trishul1767 <trishul1767@gmail.com>
2026-06-05 19:19:31 +0530

    Encapsulate model attribute in Car class

--- OOPS concept.py
+++ OOPS concept.py
@@ -1,6 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Start small. Ship something.")
 class Car:
     def __init__(self,model:str,horsepower:int):
-        self.model=model
+        self.__model=model
         self.horsepower=horsepower
         self.turn_on_engine:bool=False
         self.fuel_gauge:float=100.0
@@ -8,17 +11,17 @@ class Car:
     
     def turn_on(self):
         if self.turn_on_engine:
-            print(f'{self.model} engine is already on')
+            print(f'{self.__model} engine is already on')
         else:
             self.turn_on_engine=True
-            print(f'{self.model} engine is now on')
+            print(f'{self.__model} engine is now on')
      
     def turn_off(self):
         if self.turn_on_engine:
             self.turn_on_engine=False
-            print(f'{self.model} engine is now off')
+            print(f'{self.__model} engine is now off')
         else:
-            print(f'{self.model} engine is already off')
+            print(f'{self.__model} engine is already off')
 
     def run_lap(self,lap:int):
         if self.turn_on_engine:
@@ -26,40 +29,46 @@ class Car:
                 if self.fuel_gauge>=lap*5:
                     self.fuel_gauge-=lap*5
                     self.laps_R-=lap
-                    print(f'{self.model} completes {lap} lap(s). Fuel gauge: {self.fuel_gauge}%')
+                    print(f'{self.__model} completes {lap} lap(s). Fuel gauge: {self.fuel_gauge}%')
                 else:
-                    print(f'{self.model} does not have enough fuel to complete {lap} lap(s).')
+                    print(f'{self.__model} does not have enough fuel to complete {lap} lap(s).')
             else:
                 print(f"There are only {self.laps_R} laps remaining")
         else:
-            print(f'{self.model} engine is off. Please turn it on to run laps.')
+            print(f'{self.__model} engine is off. Please turn it on to run laps.')
 
     def __str__(self):
-        return f'Car(model = {self.model}, horsepower = {self.horsepower})'
+        return f'Car(model = {self.__model}, horsepower = {self.horsepower})'
     
     def __repr__(self):
-        return f'Car(model = {self.model}, horsepower = {self.horsepower})'
+        return f'Car(model = {self.__model}, horsepower = {self.horsepower})'
     
     def fuel_up(self,x:float):
         self.fuel_gauge+=x
-        print(f'{self.model} fuel gauge is now full.')
+        print(f'{self.__model} fuel gauge is now full.')
     
     def check_fuel(self):
-        print(f'{self.model} fuel gauge: {self.fuel_gauge}%')
+        print(f'{self.__model} fuel gauge: {self.fuel_gauge}%')
 
     def laps_remaining(self):
         print(f"Number of laps remained are {self.laps_R}")
+        
+    def setter(self,name):
+        self.__model=name
+    def getter(self):
+        return self.__model
 
 z=Car('m340i',250)
 z.turn_on()
 z.run_lap(10)
-
-y=Car('Yangwang U8', 1000)
-print(y)
 z.run_lap(5)
 z.laps_remaining()
 z.check_fuel()
 z.run_lap(2)
+z.setter('i20')
+print(z.getter())
+z.setter('m3')
+print(z.getter())
 
 
 
